8ec6c659b8dc43d9a5d82fa2b8260df32a9695ea
Jan Moxter <jan.moxter@innobix.com>
2016-04-25 21:39:39 -0500

    - adding entrypoint script part for jaas

--- application-server/docker-entrypoint.sh
+++ application-server/docker-entrypoint.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-
 set -e
 
 if ! type -- "$1" &>/dev/null; then
     set -- java -jar "-Djava.io.tmpdir=$TMPDIR" "$JETTY_HOME/start.jar" "$@"
+fi
+
+echo "$JAAS_APPNAME"
+
+if [ "$JAAS_APPNAME" ]; then
+    /bin/echo "$JAAS_APPNAME" {  > "$JETTY_BASE/etc/login.conf"
+    /bin/echo org.efaps.jaas.efaps.UserLoginModule >> "$JETTY_BASE/etc/login.conf"
+    /bin/echo SUFFICIENT >> "$JETTY_BASE/etc/login.conf"
+    /bin/echo debug=true >> "$JETTY_BASE/etc/login.conf"
+    /bin/echo jaasSystem="\""eFaps"\";" >> "$JETTY_BASE/etc/login.conf"
+    /bin/echo "};" >> "$JETTY_BASE/etc/login.conf"
 fi
 
 exec "$@"
